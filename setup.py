07ea7374cc8ea95226bb4a7ecdf85933551a3ce9
Zakaria BEKKAR <zakaria.bekkar13@gmail.com>
2021-07-25 11:02:19 +0200

    mod setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
                     'yake',
                     'keybert',
                     'gensim==3.8.3',
-                    'en_core_web_sm @ git+https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0.tar.gz',
-                    'fr_core_news_sm @ git+https://github.com/explosion/spacy-models/releases/download/fr_core_news_sm-3.0.0/fr_core_news_sm-3.0.0.tar.gz',
+                    'en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0.tar.gz',
+                    'fr_core_news_sm @ https://github.com/explosion/spacy-models/releases/download/fr_core_news_sm-3.0.0/fr_core_news_sm-3.0.0.tar.gz',
                     ], #external packages as dependencies
 )
